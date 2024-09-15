e940f52ce195484d61849207f7317ac891bd9f41
AricRedemption <aricredemption@gmail.com>
2024-09-15 10:47:07 +0800

    chore: update package id and api data

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -10,27 +10,27 @@ function getCoinInfoList(name = "", address = "") {
 }
 
 function getCoinConfig(coinType: string) {
-  // return nemoApi<CoinConfig>("/api/v1/market/config/detail").get({
-  //   coinType,
-  // })
-  return {
-    yieldFactoryConfigId:
-      "0x9bdd4da497e1741692fc16a92f0ac105d73f0316fb096ff9abd690511c3d9e01",
-    syStructId:
-      "0x93cbfddd901ef21e2b8c237198d57cd921fefdc8d735a18edd2afa33d302b68f",
-    yieldTokenStoreId: "",
-    tokenConfigId:
-      "0x0b4672e4ac2c3c6b6ad8071d4945ec86efb43e3e8e9dc218b282ed2035c54b43",
-    ytStructId:
-      "0x95b33e9e47ac1cbfd487f30bdc5987250445c08e90ab054f5593fdd17635796a",
-    ptStructId:
-      "0x024896b3415ac5536c21f21541505b8145ea374b8cd6fe22afb686467376d3b4",
+  return nemoApi<CoinConfig>("/api/v1/market/config/detail").get({
     coinType,
-    marketStateId:
-      "0x3a44223c9a0626b1c8983a230d3fd904f30ed89d583eac5a12571e18e33a59fc",
-    marketConfigId:
-      "0x0c87654918508e6177cebb22b95e462744e42cfa6d38b21d522ec2e14bc5e741",
-  } as CoinConfig
+  })
+  // return {
+  //   yieldFactoryConfigId:
+  //     "0x9bdd4da497e1741692fc16a92f0ac105d73f0316fb096ff9abd690511c3d9e01",
+  //   syStructId:
+  //     "0x93cbfddd901ef21e2b8c237198d57cd921fefdc8d735a18edd2afa33d302b68f",
+  //   yieldTokenStoreId: "",
+  //   tokenConfigId:
+  //     "0x0b4672e4ac2c3c6b6ad8071d4945ec86efb43e3e8e9dc218b282ed2035c54b43",
+  //   ytStructId:
+  //     "0x95b33e9e47ac1cbfd487f30bdc5987250445c08e90ab054f5593fdd17635796a",
+  //   ptStructId:
+  //     "0x024896b3415ac5536c21f21541505b8145ea374b8cd6fe22afb686467376d3b4",
+  //   coinType,
+  //   marketStateId:
+  //     "0x3a44223c9a0626b1c8983a230d3fd904f30ed89d583eac5a12571e18e33a59fc",
+  //   marketConfigId:
+  //     "0x0c87654918508e6177cebb22b95e462744e42cfa6d38b21d522ec2e14bc5e741",
+  // } as CoinConfig
 }
 
 export function useCoinInfoList(name = "", address = "") {
