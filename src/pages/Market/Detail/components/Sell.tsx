87ee94d9398d92a1067114b9228b4beae2bb4b94
xgdzy <1309475275@qq.com>
2025-01-03 17:18:21 +0800

    fix: sell

--- src/pages/Market/Detail/components/Sell.tsx
+++ src/pages/Market/Detail/components/Sell.tsx
@@ -154,9 +154,7 @@ export default function Sell() {
 
         const [priceVoucher] = getPriceVoucher(tx, coinConfig)
 
-        const amount = new Decimal(redeemValue)
-          .mul(10 ** coinConfig.decimal)
-          .toString()
+        const amount = new Decimal(redeemValue).toString()
 
         const swapFn = tokenType === "pt" ? swapExactPtForSy : swapExactYtForSy
         const syCoin = swapFn(tx, coinConfig, amount, pyPosition, priceVoucher)
