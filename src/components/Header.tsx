09cd2770fde5700c4508cea059af827607a3142d
AricRedemption <aricredemption@gmail.com>
2025-01-07 11:57:04 +0800

    chore: update dropdown menu for account actions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,8 @@ import { cn } from "@/lib/utils"
 import { truncateStr } from "@/lib/utils"
 import { ChevronDown, LayoutGrid } from "lucide-react"
 import { useToast } from "@/components/Toast"
-import { useEffect, useRef, useState } from "react"
+import { useState } from "react"
 import { Link, useLocation } from "react-router-dom"
-
 
 import { ConnectModal, useWallet } from "@nemoprotocol/wallet-kit"
 import { motion } from "framer-motion"
@@ -20,30 +19,9 @@ export default function Header({ className }: { className?: string }) {
   const toast = useToast()
   const location = useLocation()
   // const { accounts } = useWallet()
+  const { account: currentAccount, disconnect } = useWallet()
   const [open, setOpen] = useState(false)
-  const { account: currentAccount } = useWallet()
   const [isOpen, setIsOpen] = useState(false)
-  const [isDrop, setIsDrop] = useState(false)
-  // const { mutate: switchAccount } = useSwitchAccount()
-  const { disconnect, connected } = useWallet()
-
-  const subNavRef = useRef<HTMLDivElement>(null)
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      subNavRef.current &&
-      !subNavRef.current.contains(event.target as Node)
-    ) {
-      setIsDrop(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [connected])
 
   return (
     <header className={cn("h-16 shrink-0", className)}>
@@ -160,65 +138,34 @@ export default function Header({ className }: { className?: string }) {
               </DropdownMenuContent>
             </DropdownMenu>
           </span>
-          {/* <Network /> */}
           {currentAccount?.address ? (
-            <div className="relative" ref={subNavRef}>
-              <div
-                onClick={() => setIsDrop((isDrop) => !isDrop)}
-                className="flex items-center gap-x-1 bg-[#0E0F16] px-3 py-2 rounded-full cursor-pointer"
-              >
-                <span>{truncateStr(currentAccount?.address || "", 4)}</span>
-                <ChevronDown className="size-4" />
-              </div>
-              {isDrop && (
-                <ul className="absolute rounded-lg w-40 right-0 mt-1 overflow-hidden z-10">
-                  <li
-                    className="cursor-pointer bg-[#0E0F16] px-4 py-2 text-white/50 hover:text-white w-full"
-                    onClick={() => {
-                      disconnect()
-                    }}
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex items-center gap-x-1 border-none outline-none">
+                <span className="text-white">{truncateStr(currentAccount?.address || "", 4)}</span>
+                <ChevronDown className="size-3 mt-1" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-[#0E0F16] border-none min-w-[140px]">
+                <DropdownMenuItem>
+                  <button
+                    onClick={() => disconnect()}
+                    className="px-2 py-1.5 hover:bg-[#131520] text-white hover:text-[#5D94FF] cursor-pointer text-center w-full h-8"
                   >
                     Disconnect
-                  </li>
-                  <li
-                    className="cursor-pointer bg-[#0E0F16] px-4 py-2 text-white/50 hover:text-white w-full"
+                  </button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <button
                     onClick={() => {
-                      navigator.clipboard.writeText(
-                        currentAccount?.address || "",
-                      )
+                      navigator.clipboard.writeText(currentAccount?.address || "")
                       toast.success("Address copied to clipboard")
-                      setIsDrop(false)
                     }}
+                    className="px-2 py-1.5 hover:bg-[#131520] text-white hover:text-[#5D94FF] cursor-pointer text-center w-full h-8"
                   >
                     Copy Address
-                  </li>
-                  {/* TODO: Add switch account feature */}
-                  {/* {accounts &&
-                    accounts
-                      .filter(
-                        (account) =>
-                          account.address !== currentAccount?.address,
-                      )
-                      .map((account) => (
-                        <li
-                          key={account.address}
-                          className="cursor-pointer bg-[#0E0F16] px-4 py-2 text-white/50 hover:text-white w-full"
-                          onClick={() => {
-                            // switchAccount(
-                            //   { account },
-                            //   {
-                            //     onSuccess: () =>
-                            //       console.log(`switched to ${account.address}`),
-                            //   },
-                            // )
-                          }}
-                        >
-                          {truncateStr(account?.address || "", 4)}
-                        </li>
-                      ))} */}
-                </ul>
-              )}
-            </div>
+                  </button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           ) : (
             <ConnectModal
               theme="dark"
