93aee00080eead22f2822d9fd9bbed5439907a6f
AricRedemption <aricredemption@gmail.com>
2025-01-23 18:45:08 +0800

    feat: add console log for baseUrl in nemoApi function to aid in debugging

--- src/queries/request.ts
+++ src/queries/request.ts
@@ -77,6 +77,7 @@ const nemoRequest = <T>(url: string, options: RequestOption): Promise<T> =>
   })
 
 export const nemoApi = <T>(path: string) => {
+  console.log("baseUrl", baseUrl)
   return {
     get: (params?: OptionParams, headers?: Headers) =>
       nemoRequest<T>(`${baseUrl}${path}`, {
